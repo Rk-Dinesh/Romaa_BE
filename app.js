f57e1fc290842062742208aa88a81f0c288044e8
Rk-Dinesh <rdkumar881@gmail.com>
2026-02-09 20:13:26 +0530

    Origin

--- app.js
+++ app.js
@@ -54,10 +54,33 @@ connectDB();
 
 //middleware
 
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "http://localhost:3000",process.env.FRONTEND_URL], // 👈 ALLOW ONLY YOUR FRONTEND URL
+//     credentials: true, // 👈 ALLOW COOKIES
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  process.env.FRONTEND_URL
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000",process.env.FRONTEND_URL], // 👈 ALLOW ONLY YOUR FRONTEND URL
-    credentials: true, // 👈 ALLOW COOKIES
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
